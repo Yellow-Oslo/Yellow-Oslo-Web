5030523318e461b37a25e99d7149984807d37853
Jacob Clark <clark.3289@osu.edu>
2023-03-24 11:53:39 -0400

    Catalog

--- src/Products/Products.tsx
+++ src/Products/Products.tsx
@@ -1,7 +1,31 @@
+import data from "./data";
 function Products(){
     return (
-        <div>
-            <h2>Products</h2>
+        <div className="content">
+            <ul className="products">
+                {data.products.map((product) => (
+                    <li>
+                        <div className="product">
+                            <img
+                              className="product-image"
+                              src={product.imageUrl}
+                              alt="product"
+                              />
+                        <div className="product-name">
+                            <a href="product.html">{Products.name}</a>
+                            </div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">{product.brand}</div>  
+                            <div className="product-rating">
+                                {product.rating} Stars ({product.numberOfReviews} reveiws)       
+                        </div>
+                      </div>
+                    </li>
+                
+                
+                
+                ))};
+            </ul>
         </div>
     );
 }
