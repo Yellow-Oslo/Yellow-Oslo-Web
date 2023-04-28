4c39bf0788be7c682c44b858cc0fcb7ed5041996
Jacob Clark <clark.3289@osu.edu>
2023-04-28 15:27:49 -0400

    Lab5

---
+++ FrontEnd/src/App.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('The main app', () => {
+
+  it('should render the Orders link', () => {
+      render(<App />);
+      expect(screen.getByText('Orders')).toBeInTheDocument();
+  });
+
+    it('should render the Payment link', () => {
+        render(<App />);
+        expect(screen.getByText('Payment')).toBeInTheDocument();
+    });
+
+    it('should render the Home link', () => {
+      render(<App />);
+      expect(screen.getByRole('link', { name: "Home" })).toBeInTheDocument();
+  });
+
+  it('should render the Home header', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+});
+
+
+});
+
+
+
